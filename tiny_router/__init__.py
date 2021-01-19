4c7a4738b2ea1c0f5861a5608d0e882402d6cf5e
nekonoshiri <gexira.halen.toms@gmail.com>
2021-01-19 14:21:21 +0900

    Change to use `from .x import X as X` to support mypy --no-implicit-reexport

--- tiny_router/__init__.py
+++ tiny_router/__init__.py
@@ -1,4 +1,6 @@
-from .exceptions import MethodNotAllowed, ResourceNotFound, RouteNotFound
-from .router import Router
-from .simple import SimpleRouter
-from .simple_regex import SimpleRegexRouter
+from .exceptions import MethodNotAllowed as MethodNotAllowed
+from .exceptions import ResourceNotFound as ResourceNotFound
+from .exceptions import RouteNotFound as RouteNotFound
+from .router import Router as Router
+from .simple import SimpleRouter as SimpleRouter
+from .simple_regex import SimpleRegexRouter as SimpleRegexRouter
