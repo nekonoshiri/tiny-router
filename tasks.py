4795715c7f8483c06a940bb1a9f00e2adf67d5ea
nekonoshiri <gexira.halen.toms@gmail.com>
2021-01-25 14:44:12 +0900

    Simplify tasks.py

--- tasks.py
+++ tasks.py
@@ -1,29 +1,12 @@
 import invoke
 
-module_name = "tiny_router"
-src = "tiny_router"
-tests = "tests"
-examples = "examples"
 
-
-@invoke.task
-def lint(c):
-    c.run(f"isort {src} {tests} {examples}")
-    c.run(f"black {src} {tests} {examples}")
-    c.run(f"flake8 {src} {tests} {examples}")
-    c.run(f"mypy {src} {examples}")
-
-
-@invoke.task
-def test(c):
-    c.run(f"pytest {tests}")
-
-
-@invoke.task
-def example(c):
-    c.run(f"pytest {examples}")
-
-
-@invoke.task
-def cov(c):
-    c.run(f"pytest --cov={module_name} --cov-branch tests")
+@invoke.task()
+def check(c):
+    """Run formatting, linting and testing."""
+    c.run("isort tiny_router tests examples")
+    c.run("black tiny_router tests examples")
+    c.run("flake8 tiny_router tests examples")
+    c.run("mypy tiny_router examples")
+    c.run("pytest examples")
+    c.run("pytest --cov=tiny_router --cov-branch tests")
